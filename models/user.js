bebd4b0966f4a71ceb9abb607382906a76a3ccd7
Inna Tsymbal <inna-tsymbal2001@yandex.ru>
2023-10-02 11:23:04 +0300

    14 спринт 11 автотест

--- models/user.js
+++ models/user.js
@@ -47,12 +47,12 @@ userSchema.statics.findUserByCredentials = function findByCredits(email, passwor
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        next(new UnauthorizedError('Неправильные почта или пароль'));
+        return next(new UnauthorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            next(new UnauthorizedError('Неправильные почта или пароль'));
+            return next(new UnauthorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
